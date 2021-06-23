e84816bf93a8481061a3d6e1d1e2c7ce5ebc801f
adaskin <3620717+adaskin@users.noreply.github.com>
2021-06-23 14:52:23 +0300

    Update vqpmqubo.py

--- vqpmqubo.py
+++ vqpmqubo.py
@@ -11,6 +11,100 @@ import numpy as np
 import matplotlib.pyplot as plt
 from vqpmUtilities import   prepareNewState
 
+############################################
+#################################################################
+def calculate_block_prob(psi,qubits):
+    '''computes probabilities for the states of a given qubit group.
+      psi: a quantum state.
+      qubits:the set of qubits such as [1 3 5]
+    **the order of the qubits |1,2,3,4,..n>;
+    '''
+    
+    n = len(psi)
+
+    logn = int( np.log2(n));   
+    nq = len(qubits)
+    f = np.zeros(2**nq);
+    
+    for j in range(n):
+        jbits = bin(j)[2:].zfill(logn)
+        ind = 0
+        for q in range(nq):
+            if jbits[qubits[q]-1] == '1':
+                ind += 2**(nq-q-1)
+      
+
+        f[ind] += np.real(np.abs(psi[j])**2)
+    return f; 
+############################################
+#################################################################
+def calculate_prob(psi,qubit):
+    '''computes probabilities for the states of a given qubit.
+     psi: a quantum state.
+     qubit: index of the qubit, the order of the qubits |1,2,3,4,..n>;
+     '''
+    n = len(psi)
+
+    logn = int( np.log2(n));   
+
+    f = np.zeros(2);
+    qmask = (1<<logn-qubit)
+    #for k in range(0, len(psi)) #the columns of the input
+    for j in range(n):
+        
+        ind = 0;
+        index = ((j & qmask) != 0);
+        ind = ind + index;   
+       
+        f[ind] = f[ind]+np.real(np.abs(psi[j])**2);
+    return f; 
+    
+############################################
+#################################################################
+def prepareNewState(outVec, n,qStates, pdiff, precision):
+    '''        
+     based on probabilities of the qubits generates new state
+     measures qubits 1 to n
+     prepares a state by combining them
+    ----------
+    outVec: a quantum state
+    n: #qubits
+    qStates: given qubit states
+    pdiff: necessary prob diff between 1 and 0 to assume qubit 1 or 0
+    Returns a state, and new qStates
+    
+    '''
+    p = precision      #precision in probs
+ 
+  
+    state = [complex(1)]
+    
+    for q in range(1,n+1):
+        if q in qStates:
+            stateQ = qStates[q]
+        else:
+            stateQ = calculate_prob(outVec, q)
+           
+            stateQ = np.round(stateQ,p);
+        
+            if(stateQ[0]>stateQ[1]+pdiff):
+                stateQ[1]= 0
+                stateQ[0] = 1
+                qStates[q]=stateQ
+                
+            elif stateQ[0]+pdiff<stateQ[1]:
+                stateQ[0]=0
+                stateQ[1] = 1
+                
+                qStates[q]=stateQ
+            else:
+                stateQ = np.sqrt(stateQ)
+      
+        print("q:", q, stateQ)
+        state = np.kron(state, stateQ)    
+    return state,qStates  
+############################################
+
 def vqpmForQubo(u, n, maxiter, iexpected,  pdiff, precision):
     '''
     measures the state at each iteration
